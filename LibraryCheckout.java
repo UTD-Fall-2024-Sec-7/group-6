c4296d3ebf4df0a94d12bcd64794dcfc93bdb4b4
sanjush666 <sxl210092@utdallas.edu>
2024-11-19 09:07:58 -0600

    LibraryCheckout.java

--- LibraryCheckout.java
+++ LibraryCheckout.java
@@ -14,6 +14,7 @@ public class LibraryCheckout {
             // Add the book to the checkedOutBooks map using its ID as the key
             checkedOutBooks.put(book.getID(), book);
             user.balance -= book.getPrice;
+            user.checkouBook();
             System.out.println("Checkout successful.");
             return true; // Indicate that the checkout was successful
             
